b6e9f175d4f88bf4f15cbf52823edb5f87f217f5
99zarka <mohamedmanchestrawy@gmail.com>
2025-11-02 22:13:59 +0200

    Register additional models in Django admin
    Added Address, Conversation, Message, IssueReport, NotificationPreference, Notification, PaymentMethod, and Transaction models to the Django admin site for easier management.

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import (
     UserType, User, ServiceCategory, Service, Order, Payment, Review,
     TechnicianAvailability, TechnicianSkill, ProjectOffer, Complaint, Media,
-    VerificationDocument
+    VerificationDocument, Address, Conversation, Message, IssueReport,
+    NotificationPreference, Notification, PaymentMethod, Transaction
 )
 
 admin.site.register(UserType)
@@ -18,3 +19,11 @@ admin.site.register(ProjectOffer)
 admin.site.register(Complaint)
 admin.site.register(Media)
 admin.site.register(VerificationDocument)
+admin.site.register(Address)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(IssueReport)
+admin.site.register(NotificationPreference)
+admin.site.register(Notification)
+admin.site.register(PaymentMethod)
+admin.site.register(Transaction)
