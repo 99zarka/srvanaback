7e8d2c30d89bed2cc7d8cb93149553287d3e5daf
99zarka <mohamedmanchestrawy@gmail.com>
2025-11-06 18:04:27 +0200

    Use automatic parallelism in test runner
    Changed the test command to use '--parallel auto' instead of a fixed value, allowing Django to determine the optimal number of parallel test processes.

--- test_api.bat
+++ test_api.bat
@@ -1,4 +1,4 @@
 @echo off
 call .\venv\Scripts\activate.bat
-python manage.py test api.tests.test_api --verbosity 2 --noinput --keepdb --parallel 10
+python manage.py test api.tests.test_api --verbosity 2 --noinput --keepdb --parallel auto
 pause
