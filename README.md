f93582a53ac40aad6e13295c965c4fc33f3a3695
99zarka <mohamedmanchestrawy@gmail.com>
2025-12-06 20:17:36 +0200

    Test auto build

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # srvanaback
 
+
 Django backend for a platform that connects handy men with customers
--- test_api.bat
+++ test_api.bat
@@ -1,4 +1,5 @@
 @echo off
 call .\venv\Scripts\activate.bat
 python manage.py test  --verbosity 2 --noinput --keepdb  --settings=srvana.test_settings
+
 pause
